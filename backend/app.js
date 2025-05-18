f25ad35d08223ce8dba48345088fee2f11c9389b
manishbisht12 <154014370+manishbisht12@users.noreply.github.com>
2025-05-18 19:30:36 +0530

    working on it

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,38 @@
 import express from "express";
-import { config } from "dotenv";
-import cors from "cors"
-import cookieParser from "cookie-parser";
-import fileUpload from "express-fileupload";
 import { dbConnection } from "./database/dbConnection.js";
-import messageRouter from  "./router/messageRouter.js";
-import {errorMiddleware} from "./middlewares/errorMiddleware.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
 import userRouter from "./router/userRouter.js";
-import { patientRegister } from "./controller/userController.js";
+import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
-config({path: "./config/config.env"});
+config({ path: "./config.env" });
 
-app.use(cors({
-    origin:[process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods:["GET", "POST", "PUT", "DELETE"],
-    credentials:true,
-})
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
 );
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(
-    fileUpload({
+  fileUpload({
     useTempFiles: true,
-    tempFileDir:"/tmp/"
-}));
-
-app.use("/api/v1/message", messageRouter );
-app.use("/api/v1/message", patientRegister);
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
 
 dbConnection();
 
