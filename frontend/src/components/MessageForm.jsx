9026e4d4f55312eb905c9e581f19f373f47e55b4
manishbisht12 <154014370+manishbisht12@users.noreply.github.com>
2025-05-23 23:56:29 +0530

    working on it

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+
+import React, { useState } from 'react';
 
 const MessageForm = () => {
+  const[firstName, setFirstName] = useState("");
+  const[lastName, setLastName] = useState("");
+  const[email, setEmail] = useState("");
+  const[phone, setPhone] = useState("");
+  const[message, setMessage] = useState("");
+
+  const handleMessage = async(e)=>{
+    e.preventDefault();
+    try{
+      await axios.post()(
+        "http://localhost:4000/api/v1/message/send",
+        {firstName, lastName, phone,email,message},
+        {
+          withCredentails: true,
+          headers: {
+            "Content-Type" : "application/json",
+          }
+        }
+      ).then(res=>{
+        toast.sucess(res.data.message);
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPhone("");
+        setMessage("");
+      })
+    }catch(error) {}
+  }
   return (
-    <div>
-      
+    <div className="container form-component message-form">
+      <h2> Send Us A Message</h2>
+      <form onSubmit={handleMessage}>
+        <div>
+          <input
+           type = "text"
+           placeholder="First Name" 
+           value={firstName} onChange={(e)=> setFirstName(e.target.value)}
+           />
+          <input    type = "text"
+           placeholder="Last Name" 
+           value={lastName} onChange={(e)=> setlastName(e.target.value)}
+           />
+        </div>
+        <div>
+           <input
+           type = "text"
+           placeholder="Email" 
+           value={email} onChange={(e)=> setEmail(e.target.value)}
+           />
+          <input    type = "number"
+           placeholder="Phone Number" 
+           value={phone} onChange={(e)=> setPhone(e.target.value)}
+           />
+        </div>
+        <textarea rows={7} placeholder="Message" value={message} onChange={(e)=> setMessage(e.target.value)}
+          ></textarea>
+          <div style={{justifyContent: "center", alignItems:"center"}}>
+            <button type="submit">Send</button>
+          </div>
+      </form>
     </div>
   )
 }
